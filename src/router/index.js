f785337e94e5a48cc1dbcd731af667e6c9ef3894
arav-2003 <150048420+arav-2003@users.noreply.github.com>
2024-12-10 14:43:57 +0530

    page refresh resolved

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomePage from '../components/HomePage.vue';
 import CartPage from '../components/CartPage.vue';
 import ProductPage from '../components/ProductPage.vue';
@@ -11,7 +11,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
